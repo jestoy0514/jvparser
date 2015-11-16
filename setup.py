4a1affa50128fe473c3f63091cc3bf1cb68ebd71
jestoy0514 <jestoy.olazo@gmail.com>
2015-11-16 12:07:23 +0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='jvparser',
+    version='1.1',
+    packages=[''],
+    url='http://github.com/jestoy0514/jvparser',
+    license='GNU Public License version 3',
+    author='Jesus Vedasto Olazo',
+    author_email='jestoy.olazo@gmail.com',
+    description='A simple module for mathematical expression calculations.'
+)
