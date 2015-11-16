324cf93e7b71c7ddd195c2b35a7513cab4365679
jestoy0514 <jestoy.olazo@gmail.com>
2015-11-16 12:39:19 +0400

    Update test.

--- test/test_jvparser.py
+++ test/test_jvparser.py
@@ -37,5 +37,89 @@ def make_list(m_exp):
     return exp_list
 
 
+def showresult(exp_list):
+    # Declare the variable result to 0
+    result = 0
+    # This variable is for temporary storage of values from the
+    # calculations.
+    tmp_result = ''
+    # This is the list of operators the condition has to meet to
+    # be able to know whether the loop has to continue or not.
+    list_of_operators = ('**', '*', '/', '+', '-', '//')
+    while True:
+        # Declare a counter variable to count the operators avail.
+        # in the list.
+        counter = 0
+        for elem in exp_list:
+            if elem in list_of_operators:
+                counter += 1
+
+        # If the varible counter value is equal to zero, it convert
+        # the string result to float and check whether the result is
+        # a whole number or a decimal and return the result accordingly.
+        if counter == 0:
+            result = float(exp_list[0])
+            if result != 0:
+                if (result / int(result)) == 0:
+                    result = int(result)
+            elif result == 0:
+                result = int(result)
+            break
+        # Else if the counter is greater than zero. Perform the check
+        # for operators available and get the index of the elements
+        # and operators and perform the calculation and add it into
+        # the variable tmp_result.
+        if '**' in exp_list:
+            pos = exp_list.index('**')
+            tmp_result = str(float(exp_list[pos-1])**float(exp_list[pos+1]))
+        elif '//' in exp_list:
+            pos = exp_list.index('//')
+            tmp_result = str(float(exp_list[pos-1])//float(exp_list[pos+1]))
+        elif ('*' in exp_list) and ('/' in exp_list):
+            pos_mul = exp_list.index('*')
+            pos_div = exp_list.index('/')
+            if pos_mul < pos_div:
+                pos = pos_mul
+                tmp_result = str(float(exp_list[pos-1])*float(exp_list[pos+1]))
+            else:
+                pos = pos_div
+                tmp_result = str(float(exp_list[pos-1])/float(exp_list[pos+1]))
+        elif '*' in exp_list:
+            pos = exp_list.index('*')
+            tmp_result = str(float(exp_list[pos-1])*float(exp_list[pos+1]))
+        elif '/' in exp_list:
+            pos = exp_list.index('/')
+            tmp_result = str(float(exp_list[pos-1])/float(exp_list[pos+1]))
+        elif ('+' in exp_list) and ('-' in exp_list):
+            pos_add = exp_list.index('+')
+            pos_sub = exp_list.index('-')
+            if pos_add < pos_sub:
+                pos = pos_add
+                tmp_result = str(float(exp_list[pos-1])+float(exp_list[pos+1]))
+            else:
+                pos = pos_sub
+                tmp_result = str(float(exp_list[pos-1])-float(exp_list[pos+1]))
+        elif '+' in exp_list:
+            pos = exp_list.index('+')
+            tmp_result = str(float(exp_list[pos-1])+float(exp_list[pos+1]))
+        elif '-' in exp_list:
+            pos = exp_list.index('-')
+            tmp_result = str(float(exp_list[pos-1])-float(exp_list[pos+1]))
+
+        # Once the index and tmp_result is known, we have to delete the
+        # old entries and replace it with the new tmp_result base on the
+        # given index and set the tmp_result to empty string to use again
+        # in the calculation.
+        del exp_list[pos-1:pos+2]
+        exp_list.insert(pos-1, tmp_result)
+        tmp_result = ''
+
+    return result
+
+
 def test_make_list():
     assert make_list(math_exp) == ['4', '+', '5', '*', '6']
+
+
+def test_showresult():
+    assert showresult(['4', '+', '5', '*', '6']) == 34
